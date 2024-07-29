4cb042b481580856213b1fceee55a4abbfa7541d
ZahraKhamiyeva <100022878+ZahraKhamiyeva@users.noreply.github.com>
2024-07-29 21:37:30 +0400

    nm,,
    kjl

--- code_academy/app.js
+++ code_academy/app.js
@@ -1,1 +1,28 @@
-console.log("jjdnc");
+let str="Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+// let sum=0;
+// function findStringLength(str){
+//     // daxil edilən str string məlumatının neçə xarakterdən ibarət olduğunu return edən metod yazın
+//     for(i of str){
+//         sum+=1;
+//        }
+//        console.log(sum);
+// }
+// findStringLength("Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+// );
+
+
+sum=0;
+function findStringLength(str){
+    // daxil edilən str string məlumatının neçə hərfdən ibarət olduğunu return edən metod yazın
+    let characters=["?';,.@&"];
+    for(x of characters){
+        for(i of str){
+            console.log(x,i);
+        }
+        
+
+    }
+
+}
+findStringLength("Any fool can write code that a computer can understand. Good programmers write code that humans can understand."
+)
